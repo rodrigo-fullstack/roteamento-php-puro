412a6cb6f538551bff93425ff7597741e6648a56
rodrigo-backend <rodrigopereirfaria9@gmail.com>
2025-03-24 08:39:01 -0300

    feat(routes): Add routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -6,10 +6,10 @@ global $router;
 // 1 parametro é a rota, segundo é a ação inicialmente.
 
 // no 1 parâmetro realiza a verificação através de regexpressions se está entre /
-$router->get('', fn() => print 'Hello World');
+$router->get('/', fn() => print 'Hello World');
 
 // no segundo parâmetro com a função dispatch executa o callback através do método recebido no servidor e a uri
 $router->get('sobre', fn() => print 'Pagina sobre!');
 $router->get('/users/store', fn() => print 'Pagina sobre!');
-$router->get('helloWorld/{id}', 'Controller@hwid');
+$router->get('helloWorld/users/{id}/{email}', 'Controller@hwid');
 $router->get('/helloWorld', 'Controller@hw');
