d98829339d11684f56840c7a952d8a61f24e7faf
rodrigo-backend <rodrigopereirfaria9@gmail.com>
2025-03-25 18:28:29 -0300

    docs: Document core for better understanding

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -4,80 +4,126 @@ namespace App\Core;
 
 use App\Http\Request;
 
+/**
+ * Realiza o roteamento em si da aplicação. É considerado o coração da aplicação pois permite os usuários acessar os recursos do sistema.
+ */
 class Core
 {
 
+    /**
+     * Instância o roteamento passando como parâmetro o RouteCollection e o Dispatcher de maneira manual.
+     * @param \App\Core\RouteCollection $routes
+     * @param \App\Core\Dispatcher $dispatcher
+     */
     public function __construct(
-        private RouteCollection $routes = new RouteCollection(),
-        private Dispatcher $dispatcher = new Dispatcher()
-    ) { }
+        private RouteCollection $routes,
+        private Dispatcher $dispatcher,
+    ) {}
 
-    // adiciona as rotas a cada um dos arrays de rotas
-
+    /**
+     * Adiciona uma rota get com um padrão e um callback que pode ser um controlador com seu método (Controladora@método) ou closure (função anônima).
+     * @param string $pattern
+     * @param callable|string $callback
+     * @return void
+     */
     public function get(string $pattern, callable | string $callback): void
     {
-        $this->routes->add('GET', $pattern, $callback);
+        $this->routes->add('get', $pattern, $callback);
     }
 
+    /**
+     * Adiciona uma rota post com um padrão e um callback que pode ser um controlador com seu método (Controladora@método) ou closure (função anônima).
+     * @param string $pattern
+     * @param callable|string $callback
+     * @return void
+     */
     public function post(string $pattern, callable | string $callback): void
     {
         $this->routes->add('POST', $pattern, $callback);
 
     }
 
+    /**
+     * Adiciona uma rota put com um padrão e um callback que pode ser um controlador com seu método (Controladora@método) ou closure (função anônima).
+     * @param string $pattern
+     * @param callable|string $callback
+     * @return void
+     */
+    public function put(string $pattern, callable | string $callback): void
+    {
+        $this->routes->add('PUT', $pattern, $callback);
+
+    }
+
+    /**
+     * Adiciona uma rota delete com um padrão e um callback que pode ser um controlador com seu método (Controladora@método) ou closure (função anônima).
+     * @param string $pattern
+     * @param callable|string $callback
+     * @return void
+     */
+    public function delete(string $pattern, callable | string $callback): void
+    {
+        $this->routes->add('DELETE', $pattern, $callback);
+    }
+
+    /**
+     * Método executado quando a rota não existe, retornando um erro 404 not found.
+     * @return void
+     */
     public function notFound(): void
     {
         header("HTTP/1.0 404 Not Found");
         echo '404 Not Found';
     }
 
-    // public function put(string $pattern, callable | string $callback): void
-    // {
-    //     $this->routes->add('PUT', $pattern, $callback);
-
-    // }
-
-    // public function delete(string $pattern, callable | string $callback): void
-    // {
-    //     $this->routes->add('DELETE', $pattern, $callback);
-    // }
-
-    // realiza o roteamento
+    /**
+     * Analisa se é uma rota válida para ser despachada. Passa a requisição com injeção de dependência.
+     * @param \App\Http\Request $request
+     * @return void
+     */
     public function resolve(
-        // realiza injeção de dependência quando vai realizar o roteamento
         Request $request
     ): void {
-        // recupera a rota
+        // Recupera a rota correspondente ao URI; caso não esteja correspondentem, retorna um nulo.
         $route = $this->routes->match(
             $request->getMethod(), $request->getUri()
         );
 
-        // se não encontrar a rota retorna um not found
-        $route === null ? 
-            (function(){
-                $this->notFound();
-                return;
-            })() :
-            $this->dispatch($route);
-        
-
+        // Se não encontrar essa rota, retorna um erro 404 not found; se encontrar vai realizar o despache da rota.
+        $route === null ?
+        (function () {
+            $this->notFound();
+            return;
+        })() :
+        $this->dispatch($route);
 
     }
 
-    // realiza o direcionamento das rotas
+    /**
+     * Realiza o despache das rotas pelo objeto Dispatch
+     * @param object $route
+     * @param string $namespace
+     * @return mixed
+     */
     public function dispatch(object $route, string $namespace = "App\\Controllers\\"): mixed
     {
-        // retorna o dispatcher despachando a rota com o callback e a uri
+        // Retorna o dispatcher despachando a rota com o callback e a uri
         return $this->dispatcher->dispatch(
             callback: $route->callback,
             params: $this->parseParams($route->uri),
-            namespace: $namespace
+            namespace :$namespace
         );
+
     }
 
-    public function parseParams($uri){
-        
+    /**
+     * Realiza o tratamento dos parâmetros com base na uri fornecida vinda da função preg_match da regular expression de RouterCollection->match();
+     * @param mixed $uri
+     */
+    public function parseParams($uri)
+    {
+
         return array_slice($uri, 1);
-        
+
     }
 }
