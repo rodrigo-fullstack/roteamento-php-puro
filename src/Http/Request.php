4294bc95cc784080f6d565ba7d02fb2649b36bde
rodrigo-backend <rodrigopereirfaria9@gmail.com>
2025-03-25 18:33:45 -0300

    docs: Document Request for better understanding

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -1,75 +1,123 @@
 <?php
 namespace App\Http;
 
-// 1. refatorar requisição
-
+/**
+ * Armazena informações relativas à requisição do cliente.
+ */
 class Request
 {
 
+    /**
+     * Armazena a URI acessada.
+     * @var string
+     */
     private string $uri;
+
+    /**
+     * Armazena o método da requisição.
+     * @var string
+     */
     private string $method;
+
+    /**
+     * Armazena os dados da requisição (body da requisição).
+     * @var string
+     */
     private array $body;
+
+    /**
+     * Armazena os arquivos da requisição ($_FILES).
+     * @var string
+     */
     private array $files;
 
+    /**
+     * Instancia o Request com os seus devidos dados preenchidos.
+     */
     public function __construct()
     {
+        // Atribui a URI com a requisição do servidor.
         $this->uri = trim(
-            // remove os caracteres vazios ou / dos cantos do resultado da função parse_url
             parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'
         );
-        // recebe o método já minúsculo
+        // Recebe o método do servidor em minúsculo.
         $this->method = strtolower($_SERVER['REQUEST_METHOD']);
 
+        // Define o corpo da requisição.
         $this->setBody();
+
+        // Define os arquivos da requisição.
         $this->setFiles();
     }
 
-    // retorna a uri
+    /**
+     * Retorna a URI do Request.
+     * @return string
+     */
     public function getUri(): string
     {
         return $this->uri;
 
     }
 
-    // retorna o método usado
+    /**
+     * Retorna o método usado.
+     * @return string
+     */
     public function getMethod(): string
     {
         return $this->method;
     }
 
-    // recupera o body
+    /**
+     * Retorna o body da requisição.
+     * @return string
+     */
     public function getBody(): array | null
     {
         return $this->body;
     }
 
-    // verifica se há arquivo
+    /**
+     * Retorna o body da requisição.
+     * @return string
+     */
     public function hasFile($key): bool
     {
         return key_exists($key, $this->files);
     }
 
-    // recupera arquivo com a chave
+    /**
+     * Retorna o arquivo desejado pelo nome (ou chave do array).
+     * @return array 
+     * @return null 
+     */
     public function getFile($key): array | null
     {
+        // Se existe o arquivo, retorna ele mesmo; se não, retorna nulo.
         return $this->files[$key] ?? null;
     }
 
-    // recupera os dados (body) da requisição
+    /**
+     * Define o body da requisição.
+     */
     public function setBody()
     {
+        // Define com base no método da requisição.
         $this->body = match ($this->method) {
-            // recebe todos os dados de get
+            // Recebe todos os dados de get
             'get' => $_GET,
-            // recebe todos os dados de post
+            // Recebe todos os dados de post
             'post' => $_POST,
-            // recebe todos os dados de put e delete decodificando um json
+            // Recebe todos os dados de put e delete decodificando um json
             'put', 'delete' => json_decode(file_get_contents('php://input'), true) ?? [],
             default         => []
         };
     }
 
-    // define os arquivos pelo global $_FILES
+    /**
+     * Define os arquivos da requisição.
+     */
     public function setFiles()
     {
         $this->files = $_FILES ?? [];
