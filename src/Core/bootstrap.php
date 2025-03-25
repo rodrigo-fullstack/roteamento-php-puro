7667b862948411ca47bc8506006d98df9ecde2df
rodrigo-backend <rodrigopereirfaria9@gmail.com>
2025-03-25 18:32:52 -0300

    refactor: Inject Dependency manually

--- src/Core/bootstrap.php
+++ src/Core/bootstrap.php
@@ -5,9 +5,14 @@ declare (strict_types = 1);
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 use App\Core\Core;
+use App\Core\Dispatcher;
+use App\Core\RouteCollection;
 use App\Http\Request;
 
-$router = new Core();
+$router = new Core(
+    new RouteCollection(),
+    new Dispatcher()
+);
 require __DIR__ . '/../Http/routes.php';
 
 // testar sistema de rotas
